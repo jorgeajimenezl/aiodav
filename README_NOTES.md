7534da01ce16975fbafd9058e0a805926854f403
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2023-04-27 09:38:49 -0400

    Changed to pyproject.toml specification

--- README_NOTES.md
+++
@@ -1,5 +0,0 @@
-### Version 0.1.0
-* Refactoring [webdavclient3](https://github.com/ezhov-evgeny/webdav-client-python-3) and making it works
-* Replace old synchronous methods by asynchronous methods
-* Added support for progress callback
-* Added typing support
--- setup.py
+++ setup.py
@@ -1,39 +1,4 @@
-import re
-from setuptools import setup, find_packages
+import setuptools
 
-with open("aiodav/__init__.py", "r") as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
-    ).group(1)
-
-with open("requirements.txt", "r") as file:
-    INSTALL_REQUIRES = file.readlines()
-
-with open("README.md") as readme:
-    setup(
-        name="aiodav",
-        version=version,
-        description="A Python Async WebDAV Client",
-        long_description=readme.read(),
-        long_description_content_type="text/markdown",
-        license="MIT License",
-        author="Jorge Alejandro Jimenez Luna",
-        author_email="jorgeajimenezl17@gmail.com",
-        url="https://github.com/jorgeajimenezl/aiodav",
-        classifiers=[
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-        ],
-        keywords="webdav, client, files, internet, download, upload",
-        install_requires=INSTALL_REQUIRES,
-        packages=find_packages(),
-    )
+if __name__ == "__main__":
+    setuptools.setup(name="aiodav")
